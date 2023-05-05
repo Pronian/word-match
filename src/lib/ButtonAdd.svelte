5ecf8e684523e393dc59047803bb2f9d1670770a
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2023-05-05 21:54:39 +0300

    Added icon lib

---
+++ src/lib/ButtonAdd.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import BoxContainer from "$lib/BoxContainer.svelte";
+	import IconPlus from "~icons/basil/plus-solid";
+
+	export let title: string;
+</script>
+
+<BoxContainer --color="green">
+	<button {title}>
+		<IconPlus width="32" height="32"/>
+	</button>
+</BoxContainer>
+
+<style>
+	button {
+		display: block;
+		outline: none;
+		border: none;
+		border-radius: 0;
+		padding: 0;
+		line-height: 0;
+		color: white;
+		background-color: var(--color);
+	}
+</style>
---
+++ src/routes/word-pools/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import ButtonAdd from '$lib/ButtonAdd.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<ButtonAdd title="Add Word Pool" />
