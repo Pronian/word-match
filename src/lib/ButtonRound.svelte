08f62060b072a42d8f83d9ed5a228b41be9584ed
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2023-05-08 13:40:53 +0300

    Added round button component

---
+++ src/lib/ButtonRound.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import { onDestroy, createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	export let depth = 5;
+	let className = '';
+	export { className as class };
+	export let classBtn = '';
+
+	let dynamicDepth = depth + 'px';
+	let depthChangeTimeout = 0;
+
+	function click(depthDiff: number, duration: number) {
+		dynamicDepth = (depth + depthDiff) + 'px';
+		clearTimeout(depthChangeTimeout);
+
+		setTimeout(() => {
+			dispatch('click');
+		}, duration);
+
+		depthChangeTimeout = setTimeout(() => {
+			dynamicDepth = depth + 'px';
+		}, duration * 2);
+	}
+
+	onDestroy(() => {
+		clearTimeout(depthChangeTimeout);
+	});
+</script>
+
+<div class="cont {className}" style:--depth={dynamicDepth}>
+	<div class="butt-shape"/>
+	<div class="butt-wrapper">
+		<button class={classBtn} on:click={() => click(-2, 200)}>
+			<slot/>
+		</button>
+	</div>
+</div>
+
+<style>
+	.cont {
+		--radius : 30px;
+		position: relative;
+	}
+
+	.butt-shape, button {
+		display: block;
+		border-radius: var(--radius);
+	}
+
+	.butt-shape {
+		position: absolute;
+		top: 0;
+		right: 0;
+		left: 0;
+		bottom: 0;
+		background-color: var(--c-bg-dark);
+	}
+
+	.butt-wrapper {
+		border-radius: var(--radius);
+		border: 3px solid var(--c-bg-light);
+		background-color: var(--c-bg-light);
+		transform: translateY(calc(var(--depth) * -1));
+		transition: transform 200ms ease-in-out;
+	}
+
+	button {
+		padding: 1.2ch;
+		color: var(--c-text-dark);
+		font-size: 18px;
+		font-weight: var(--weight-bold);
+		border: 3px solid var(--c-theme);
+		background-color: var(--c-bg-light);
+		outline: none;
+	}
+
+	button:focus-visible {
+		background-color: var(--c-theme);
+	}
+
+	button:not(:disabled) {
+		cursor: pointer;
+	}
+</style>
